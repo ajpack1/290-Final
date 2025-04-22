186d4bd575d753bff80dfd162f216394a182f9d7
zoyanashita <rahmanzn2016@gmail.com>
2025-04-22 11:38:54 -0400

    Adding gravity and changing player position to be relative to the ground

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Oculus.Interaction;
 using Oculus.Interaction.PoseDetection;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 // This is a script to move the character forwards when a pose is given 
 public class Controller : MonoBehaviour
@@ -14,7 +15,10 @@ public class Controller : MonoBehaviour
     private CharacterController characterController;
       // reference to the CenterEyeAnchor, to access the headsets forward eye direction. 
     public Transform centerEyeAnchor;
-    
+    // reference to the ground
+    public GameObject ground;
+    // original start position
+    private Vector3 startPos;
     
     // HAND TRACKING REFERENCES / VARIABLES 
     // movement speed 
@@ -23,12 +27,12 @@ public class Controller : MonoBehaviour
     private ShapeRecognizerActiveState script;
     // Gain acccess to the hand pose object we want to trigger our action 
     public GameObject handPose;
-    
-    // OLD IMPLEMENTATION VARIABLES BELOW:
-    // Public access to the game object OVR setup - we will use this to move the player based on a gesture
-        // public GameObject headset;
-    // Vector3 to move our position 
-        // private Vector3 movePos;
+
+    // gravity and jump settings 
+    public float gravity = -9f;
+    public float jumpForce = 5f;
+    // Tracks vertical velocity for gravity & jumping
+    private float verticalVelocity = 0f;
 
 
     // Start is called before the first frame update
@@ -40,6 +44,9 @@ public class Controller : MonoBehaviour
 
         // accessing the character controller rigid body so we can modify its position, along with the player
         characterController = OVRRig.GetComponent<CharacterController>();
+
+        // saving intial start position
+        startPos = characterController.transform.position;
     }
 
     // Update is called once per frame 
@@ -50,48 +57,50 @@ public class Controller : MonoBehaviour
         {
             doSomething();
         }
-
+        
+        
+        if (characterController.transform.position.y < ground.transform.position.y - 0.5f) {
+                characterController.transform.position = startPos;
+            }
     }
 
-    // This is a public method we put onto a gameobject - doing it this way makes things simpler
-    // It is like lego building blocks - we put this script onto an object, and then that object can be accessed
-    // which is actually our script to do something - we are then able to tell unity to run this script when
-    // it sees a hand gesture run - this also allows for the ease of script access,
-    // where one script can access another - ari
-
-    // that was the original implementation, now we are instead going to access the CenterEyeAnchor vector3 position
+   
+    // we are  going to access the CenterEyeAnchor vector3 position
     // to determine where the headset is facing. this way, we can track active input on the player's direction, and 
     // customize movement so that the player moves in the direction they are facing - zoya
     public void doSomething()
     {
-        // OLD IMPLEMENTATION 
-        // movePos = headset.transform.position;
-        // movePos.z = movePos.z + speed;
-        // headset.transform.position = movePos;
-
+        
         // instead of accessing the headset to transform position , we are accessing the forward
         // view of the headset, to determine the direction we should be moving in based on the 
         // direction the player is facing
         Vector3 moveDirection = centerEyeAnchor.forward;
+        
+        // zero out vertical movement so we keep movement on XZ axis 
         moveDirection.y = 0;
+        moveDirection.Normalize();
+        Vector3 horizontalMovement = moveDirection * speed;
 
-        // // we are removing any vertical components for now, so our movement remains horizontal, and height 
-        // // is only affected by the rigid body interaction. 
-        // // because of this, we also have to normalize the vectors, so they remain consistent on the plane
-        // // despite us changing the y position. 
-        // moveDirection.y = 0;
-        // moveDirection.Normalize();
+        // gravity and jump logic
+        if (characterController.isGrounded) {
+            // when grounded, reset vertical
+            verticalVelocity = 0f;
 
-        // now we move the characterController's rigid body, rather than the headset itself, so the rigid body
-        // may interact with other objects in the scene during its movement. Since we are pulling a reference
-        // we have to do a null check first. 
-        if (characterController != null) {
-            characterController.Move(moveDirection * speed * Time.deltaTime);
+            // when implementing jump trigger place here
         } else {
-            // directly modifying the position if the character controller isn't found. Also a good implementation
-            // just on its own, but ignores collisions and therefore inadequate for this project. 
-            OVRRig.transform.position += moveDirection * speed * Time.deltaTime;
+            // apply gravity 
+            verticalVelocity += gravity * Time.deltaTime;
+
+            // check if below the ground
+            
         }
+
+        Vector3 finalMovement = horizontalMovement;
+        finalMovement.y = verticalVelocity;
+
+        characterController.Move(finalMovement * Time.deltaTime);
+
+       
     }
 }
 
